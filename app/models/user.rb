5a9f392972b659e795477b5f997915cf0dc1493f
Dario Jularic <dariojularic0@gmail.com>
2023-03-01 15:58:57 +0100

    no name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :garages, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  validates :name, presence: true
+  # validates :name, presence: true
 end
