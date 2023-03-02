3f4e03b87a482c4fff326423cd9d1f9ca05741f8
Dario Jularic <dariojularic0@gmail.com>
2023-03-02 17:22:01 +0100

    some changes

--- app/javascript/controllers/button_controller.js
+++ app/javascript/controllers/button_controller.js
@@ -2,6 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="button"
 export default class extends Controller {
-  connect() {
+  static targets = [ "button", "updatebutton" ]
+
+  submit(event) {
+    alert("Button clicked!")
+    // event.preventDefault();
+    console.log("Button clicked!")
+    this.buttonTarget.disabled = true
+    this.buttonTarget.value = "Garage created!"
+    // event.target.form.submit();
+  }
+
+  update(event) {
+    // alert("Update clicked!")
+    console.log(event.submitter)
+    console.log("Update clicked!")
+    event.submitter.disabled = true
+    event.submitter.value = "Garage updated!"
   }
 }
